e36d5a4c579ad0a6df6f48e5baa31aee358b6090
mayrashizue <mayrashitzu@gmail.com>
2026-01-28 00:14:51 -0300

    python class

---
+++ clase1.py
@@ -0,0 +1,7 @@
+print("Hola, mundo")
+print("Estoy aprendiendo Python")
+nombre = "checho"
+edad = 25
+
+print(nombre)
+print(edad)
---
+++ clase2.py
@@ -0,0 +1,6 @@
+temperatura = float(input("Ingrese la temperatura corporal: "))
+
+if temperatura >= 38:
+    print("Tiene fiebre")
+else:
+    print("No tiene fiebre")
